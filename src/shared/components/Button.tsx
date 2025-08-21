67c360eff79dd18b6490bb14d519b516fc8d43c1
PC2502 <zxcv530@naver.com>
2025-08-21 23:32:56 +0900

    [Hoy-25] refactor: 공용 버튼 컴포넌트 리팩토링 (윤정환)

--- src/shared/components/Button.tsx
+++ src/shared/components/Button.tsx
@@ -1,47 +1,30 @@
-import { ReactNode } from 'react';
+import { ComponentPropsWithoutRef } from 'react';
 
 import { cn } from '../lib/cn';
 
-interface Props {
-  children: ReactNode;
-  className?: string;
-  type?: 'button' | 'submit';
+interface Props extends ComponentPropsWithoutRef<'button'> {
   variant?: 'primary' | 'secondary' | 'tertiary';
-  disabled?: boolean;
-  onClick?: () => void;
 }
 
-const Button = ({
-  children,
-  className,
-  type = 'button',
-  variant = 'primary',
-  disabled = false,
-  onClick,
-}: Props) => {
-  const getVariantStyle = () => {
-    switch (variant) {
-      case 'primary':
-        return 'bg-main text-white disabled:bg-black-700 disabled:text-gray-600 hover:bg-red';
-      case 'secondary':
-        return 'bg-transparent text-main border-1 border-main hover:border-red hover:text-red';
-      case 'tertiary':
-        return 'bg-transparent text-gray-600 border-1 border-gray-600 hover:border-gray-400 hover:text-gray-400 focus:border-red focus:text-red';
-    }
-  };
+const VARIANT_STYLE = {
+  primary: 'bg-main text-white disabled:bg-black-700 disabled:text-gray-600 hover:bg-red',
+  secondary: 'bg-transparent text-main border-1 border-main hover:border-red hover:text-red',
+  tertiary:
+    'bg-transparent text-gray-600 border-1 border-gray-600 hover:border-gray-400 hover:text-gray-400 focus:border-red focus:text-red',
+};
 
+const BASE_STYLE =
+  'xl:text-lg-semibold flex h-[50px] w-full max-w-[335px] cursor-pointer items-center justify-center rounded-md font-semibold whitespace-nowrap disabled:cursor-auto md:h-[55px] md:max-w-110 xl:h-[65px] xl:max-w-160';
+
+const Button = ({ type = 'button', variant = 'primary', ...props }: Props) => {
   return (
     <button
-      className={cn(
-        'xl:text-lg-semibold flex h-[50px] w-full max-w-[335px] cursor-pointer items-center justify-center rounded-md font-semibold whitespace-nowrap disabled:cursor-auto md:h-[55px] md:max-w-110 xl:h-[65px] xl:max-w-160',
-        getVariantStyle(),
-        className,
-      )}
+      className={cn(BASE_STYLE, VARIANT_STYLE[variant], props.className)}
       type={type}
-      disabled={disabled}
-      onClick={onClick}
+      disabled={props.disabled}
+      onClick={props.onClick}
     >
-      {children}
+      {props.children}
     </button>
   );
 };
