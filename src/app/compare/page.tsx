27bedabab4e8099d44f5f44f51dd8bd1ae7fe07f
KimIn <inptp1996@gmail.com>
2025-08-23 01:03:56 +0900

    [HOY-9]fix: 포크 레포지토리로 변경(김인)

--- src/app/compare/page.tsx
+++ src/app/compare/page.tsx
@@ -1,5 +1,24 @@
+// 비교 페이지
+'use client';
+
+import { useState } from 'react';
+
+import Dropdown from '@/shared/components/Dropdown';
+const CATEGORIES = [
+  { name: 'category1', value: 1 },
+  { name: 'category2', value: 2 },
+  { name: 'category3', value: 3 },
+  { name: 'category4', value: 4 },
+];
+
 const ComparePage = () => {
-  return <></>;
+  const [selected, setSelected] = useState<string | number | boolean | null>(null);
+
+  return (
+    <div>
+      <Dropdown options={CATEGORIES} onChange={(v) => setSelected(v)} />
+    </div>
+  );
 };
 
 export default ComparePage;
