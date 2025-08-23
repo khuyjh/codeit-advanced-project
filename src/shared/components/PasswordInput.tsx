28a16bbc870b62531ee5711f2de6bc75bc7eb237
PC2502 <zxcv530@naver.com>
2025-08-23 14:45:00 +0900

    [HOY-62] feat: 공용 Input 컴포넌트, 비밀번호 Input 컴포넌트 제작 (윤정환)

---
+++ src/shared/components/PasswordInput.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+import { useState } from 'react';
+
+import Input, { InputProps } from './Input';
+import VisibilityOffIcon from '../../../public/icons/VisibilityOffIcon.svg';
+import VisibilityOnIcon from '../../../public/icons/VisibilityOnIcon.svg';
+import { cn } from '../lib/cn';
+
+//기존 Input 타입에서 label을 필수처리
+type PasswordInput = Omit<InputProps, 'label'> & Required<Pick<InputProps, 'label'>>;
+
+const PasswordInput = ({ label, error, helperText, ...props }: PasswordInput) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleClickToggleVisibility = () => {
+    setIsVisible((prev) => !prev);
+  };
+
+  return (
+    <div className='relative w-full max-w-[335px] md:max-w-110 xl:max-w-160'>
+      <Input
+        error={error}
+        type={isVisible ? 'text' : 'password'}
+        label={label}
+        helperText={helperText}
+        {...props}
+      />
+      {/* helperText가 있을 때와 없을 때 전체 높이가 달라져 버튼 위치를 조건부 스타일링 처리 */}
+      <div
+        className={cn('absolute top-[55%] right-5', {
+          'top-[40%] right-5 xl:top-[42%]': helperText,
+        })}
+      >
+        {isVisible ? (
+          <button className='cursor-pointer' onClick={handleClickToggleVisibility}>
+            <VisibilityOnIcon />
+          </button>
+        ) : (
+          <button className='cursor-pointer' onClick={handleClickToggleVisibility}>
+            <VisibilityOffIcon />
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default PasswordInput;
