b9848a811017632f5e0ad03d305d7d963f92ebe2
KimIn <inptp1996@gmail.com>
2025-08-23 17:15:57 +0900

    [HOY-9]chore: 브랜치 정상화(김인)

--- src/app/compare/page.tsx
+++ src/app/compare/page.tsx
@@ -1,25 +1,6 @@
 // 비교 페이지
-'use client';
-
-import { useState } from 'react';
-
-import Dropdown from '@/shared/components/Dropdown';
-
-const CATEGORIES = [
-  { name: 'category1', value: 1 },
-  { name: 'category2', value: 2 },
-  { name: 'category3', value: 3 },
-  { name: 'category4', value: 4 },
-];
-
 const ComparePage = () => {
-  const [selected, setSelected] = useState<string | number | boolean | null>(null);
-
-  return (
-    <div>
-      <Dropdown options={CATEGORIES} onChange={(v) => setSelected(v)} />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default ComparePage;
