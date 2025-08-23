ca20254c97ea5c5a76a3a00e6b9fd3ed8d51589f
이관현\Luganic <rhksgus509@gmail.com>
2025-08-23 22:03:17 +0900

    [HOY-26] refactor: 리뷰 피드백 반영,관심사 분리 및 반응형 수정(이관현)

--- src/shared/components/card/statisticsCard.tsx
+++ src/shared/components/card/statisticsCard.tsx
@@ -1,14 +1,28 @@
 import { Star, Heart, MessageSquare } from 'lucide-react';
 import React from 'react';
 
+import { cn } from '@/shared/lib/cn';
+import { formatNumber } from '@/shared/utils/formatters';
+
 interface StatisticsCardProps {
   title: string;
   value: string | number;
   iconType: 'star' | 'heart' | 'message';
   comparisonText: string;
+  className?: string;
 }
+const CARD_BASE_STYLE =
+  'bg-black-800 border-black-700 border flex h-[82px] w-full flex-col rounded-[12px] px-[20px] py-[20px] md:h-[169px] md:px-[40px] md:gap-y-[15px] md:py-[30px] xl:h-[190px] xl:px-[70px] xl:py-[30px]';
 
-const StatisticsCard = ({ title, value, iconType, comparisonText }: StatisticsCardProps) => {
+const CONTENT_ALIGNMENT_STYLE = 'justify-center items-start md:justify-center md:items-center';
+
+const StatisticsCard = ({
+  title,
+  value,
+  iconType,
+  comparisonText,
+  className,
+}: StatisticsCardProps) => {
   const iconMap = {
     star: { component: <Star fill='currentColor' />, colorClass: 'text-yellow' },
     heart: { component: <Heart fill='currentColor' />, colorClass: 'text-red' },
@@ -16,15 +30,10 @@ const StatisticsCard = ({ title, value, iconType, comparisonText }: StatisticsCa
   };
 
   const { component: IconComponent, colorClass } = iconMap[iconType];
-  const formattedValue = typeof value === 'number' ? value.toLocaleString('ko-KR') : value; //한국식 숫자 단위
-
-  const CARD_BASE_STYLE =
-    'bg-black-800 border-black-700 border flex h-[82px] w-[335px] flex-col rounded-[12px] px-[20px] py-[20px] md:h-[169px] md:w-[218px] md:px-[40px] md:py-[30px] xl:h-[190px] xl:w-[300px] xl:px-[70px] xl:py-[30px]';
-
-  const CONTENT_ALIGNMENT_STYLE = 'justify-center items-start md:justify-center md:items-center';
+  const formattedValue = formatNumber(value); //한국식 숫자 단위
 
   return (
-    <div className={`${CARD_BASE_STYLE} ${CONTENT_ALIGNMENT_STYLE} md:gap-y-[15px]`}>
+    <div className={cn(CARD_BASE_STYLE, CONTENT_ALIGNMENT_STYLE, className)}>
       {/* 제목 및 값 섹션 */}
       <div className='mb-[5px] flex items-center justify-between space-x-2.5 md:flex-col md:items-center md:space-x-0 md:gap-y-[15px]'>
         {/* 제목 */}
